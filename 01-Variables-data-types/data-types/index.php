4e09812fcc7633d02f00c6c13d60a7d6128312a1
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-11-01 16:54:01 +0100

    :sparkles: feat: learning data-types in php

---
+++ 01-Variables-data-types/data-types/index.php
@@ -0,0 +1,51 @@
+<?php 
+
+//string
+
+$name= 'Dina Iyanuloluwa';
+$name2= 'John Doe';
+
+var_dump($name);
+
+echo '<br/>';
+
+//Integer
+
+$age = -35;
+var_dump($age);
+
+echo '<br />';
+
+//Float
+
+$rating = 4.5;
+var_dump($rating);
+echo '<br />';
+
+//Boolean 
+$isLoaded = true;
+var_dump($isLoaded);
+echo '<br />';
+
+//Array
+
+$friends = ['John', 'Jack', 'Jane'];
+
+var_dump($friends);
+echo '<br />';
+
+//Object
+$person = new stdClass();
+var_dump($person);
+echo '<br />';
+
+//Null
+
+$car = null;
+var_dump($car);
+echo '<br />';
+
+
+
+
+
