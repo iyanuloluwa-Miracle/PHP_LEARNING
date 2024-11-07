a7b5783d7b2b0c887b8af445d2e8e94e737fbac0
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-11-07 10:59:51 +0100

    feat: intro to arrays

---
+++ 02-arrays-and-iteration/01-intro-to-arrays/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+$names = array('John', 'Jack', 'Jill');
+$numbers = [1, 2, 3, 4, 5, 6];
+
+
+// echo $names[2];
+// echo $numbers[3];
+
+
+
+// Add element to array
+$numbers[] = 100;
+$numbers[] = 101;
+
+$numbers[6] = 100;
+//inspect($numbers);
