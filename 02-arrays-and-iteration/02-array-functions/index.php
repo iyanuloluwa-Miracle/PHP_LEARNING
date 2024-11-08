5b8003f72601ebbdda4ecd9bdb6cc8588ec99e9a
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-11-08 15:18:08 +0100

    feat: array functions

--- 02-arrays-and-iteration/02-array-functions/index.php
+++ 02-arrays-and-iteration/02-array-functions/index.php
@@ -1,5 +1,55 @@
 <?php
 $output = null;
+$ids = [10, 22, 15, 45, 67, 89];
+$users = ['user1', 'user2', 'user3'];
+
+//count
+$output = count($ids);
+
+//sort
+sort($ids);
+sort($users);
+
+// rsort
+
+rsort($ids);
+rsort($users);
+
+// array_push
+
+array_push($ids, 100);
+array_push($users, 'user4');
+
+// array_pop
+
+array_pop($ids);
+array_pop($users);
+
+// array_shift
+array_shift($ids);
+array_shift($users);
+
+// array_unshift
+array_unshift($ids, 100);
+array_unshift($users, 'user3');
+
+// array_slice
+$ids2 = array_slice($ids,2, 3);
+//var_dump($ids2);
+
+// array_splice
+array_splice($ids, 1,1, 'New ID');
+array_splice($users, 0,1, 'New User');
+
+
+// array_sum
+$output = 'Sum of IDs ' . array_sum($ids);
+
+// array_search
+$output = 'User 2 is at index' . array_search('user2', $users);
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -7,6 +57,7 @@ $output = null;
 
 <head>
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>PHP From Scratch</title>
@@ -22,6 +73,24 @@ $output = null;
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
             <p class="text-xl"><?= $output ?></p>
+            <h2 class="text-xl font-semibold my-4">IDs Array:</h2>
+            <p>
+            <pre>
+                    <?php print_r($ids); ?>
+
+                </pre>
+            </p>
+
+
+            <h2 class="text-xl font-semibold my-4">Users Array:</h2>
+            <p>
+            <pre>
+                    <?php print_r($users); ?>
+
+                </pre>
+            </p>
+
+
             </p>
         </div>
     </div>
