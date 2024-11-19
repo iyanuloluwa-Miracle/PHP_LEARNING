fe5f4f9fbd0a3ecb87b77173458306d962cb3f86
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-11-19 12:19:08 +0100

    feat: if statements

--- 03-control-structures/01-if-statements/index.php
+++ 03-control-structures/01-if-statements/index.php
@@ -1,10 +1,43 @@
 <?php
 
+
+$age = 16;
 // If statement
+
+// if($age >=21){
+//     // echo 'You are old enough to drink in the US';
+// }
 
 // If-Else
 
+// if ($age >= 21) {
+//     echo 'You are old enough to drink in the US';
+// }else{
+//     echo 'You are Not old enough to drink in the US';
+// }
 // Nested if statement
+if($age >=21){
+    echo 'You are old enough to drink and Vote in the US';
+
+}else{
+    if($age >=18){
+        echo 'You are old enough to vote in the US';
+    } else {
+        echo 'You are Not old enough to drink or vote in the US';
+    }
+}
+
 
 // If-Else-If
+if ($age >= 21) {
+    echo 'You are old enough to drink and Vote in the US';
+} else {
+    if($age >=18){
+        echo 'You are old enough to Vote in the US';
+    }else{
+        echo 'You are Not old enough to drink or Vote in the US';
+    }
+
+}
+
 
