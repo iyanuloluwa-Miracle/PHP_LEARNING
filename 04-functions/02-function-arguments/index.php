ae902cbd4a1777d0177e97eb939dadbbaea61cd7
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-11-26 14:27:49 +0100

    feat:parameters and arguments

---
+++ 04-functions/02-function-arguments/index.php
@@ -0,0 +1,35 @@
+<?php
+
+function add($a, $b){
+    return $a + $b;
+}
+
+
+echo add(1,2);
+echo '<br>';
+echo add(100, 250);
+
+echo '<br>';
+
+// echo add();
+
+function sayHello($name = 'World'){
+
+    return 'Hello' . $name;
+
+}
+
+echo sayHello();
+echo '<br>';
+
+function addAll(...$numbers) {
+    $total = 0;
+
+    foreach($numbers as $number){
+        $total += $number;
+    }
+
+    return $total;
+}
+
+echo addAll(1,2,3,4,5,6);
