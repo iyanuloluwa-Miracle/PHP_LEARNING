ff7eb5623b9b0c4133b412b81acc0f547636f2fe
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-12-02 13:33:43 +0100

    feat: inheritance

--- 05-oop/03-inheritance/index.php
+++ 05-oop/03-inheritance/index.php
@@ -4,7 +4,7 @@ class User
 {
   public $name;
   public $email;
-  protected $status = 'active';
+  private $status = 'active';
 
   public function __construct($name, $email)
   {
@@ -17,3 +17,29 @@ class User
     echo $this->name . ' logged in <br>';
   }
 }
+
+class Admin extends User
+{
+  public $level;
+
+  public function __construct($name, $email, $level)
+  {
+    parent::__construct($name, $email); // Call parent constructor first
+    $this->level = $level;
+  }
+
+
+  public function login(){
+    echo 'Admin' .  $this->name. ' logged in <br>';
+  }
+}
+
+$admin1 = new Admin('Tom Smith', 'tom@gmail.com', 5);
+
+$admin1->login();
+
+echo $admin1->name;
+echo $admin1->email;
+
+
+var_dump($admin1);
